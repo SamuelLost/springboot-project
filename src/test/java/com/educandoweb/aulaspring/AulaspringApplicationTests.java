5a0a6fe2a9f743c9c17f3e951da30f367da6af5a
Samuel Henrique <samuelhenriqec@alu.ufc.br>
2023-02-02 21:09:04 -0300

    Projeto criado

---
+++ src/test/java/com/educandoweb/aulaspring/AulaspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.aulaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
