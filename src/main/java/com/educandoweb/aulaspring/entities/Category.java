90741aab48ad7fbb7d7fb63e37adf876a50d07fe
Samuel Henrique <samuelhenriqec@alu.ufc.br>
2023-02-03 15:21:03 -0300

    Associações e relacionamentos, conversão do paradigma de POO para o relacional

--- src/main/java/com/educandoweb/aulaspring/entities/Category.java
+++ src/main/java/com/educandoweb/aulaspring/entities/Category.java
@@ -4,13 +4,14 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
 
 @Entity
 @Table(name = "tb_category")
@@ -23,8 +24,9 @@ public class Category implements Serializable {
     private Long id;
     private String name;
 
-    // @OneToMany
-    @Transient
+    // @Transient --> não vai ser mapeado no banco de dados
+    @JsonIgnore
+    @ManyToMany(mappedBy = "categories")
     private Set<Product> products = new HashSet<>();
 
     public Category() {
